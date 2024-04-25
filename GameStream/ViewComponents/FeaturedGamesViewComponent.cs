6a6fb909f2ab4651222e56a70d20d6f89c11c587
nil1lik <birliknil@gmail.com>
2024-04-25 16:19:40 +0300

    created view components

---
+++ GameStream/ViewComponents/FeaturedGamesViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GameStream.ViewComponents
+{
+    public class FeaturedGamesViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
