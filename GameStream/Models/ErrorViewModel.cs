e8c0dfa76e80c86d7003a92791d263021df40214
nil1lik <birliknil@gmail.com>
2024-04-25 16:19:21 +0300

    deleted error view model class

--- GameStream/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace GameStream.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
