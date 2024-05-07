d2d88259862718ccd56ca0b12ffe8077aad8ce68
nil1lik <birliknil@gmail.com>
2024-05-07 19:03:12 +0300

    created DetailPageDetailsViewModel

---
+++ GameStream/Models/DetailPageDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Business.DT0s.GameDetail;
+
+namespace GameStream.Models
+{
+    public class DetailPageDetailsViewModel
+    {
+        public DetailsPageGameDetailDto DetailsPageGameDetail { get; set; }
+        public List<DetailsPageGameDetailDto> DetailsPageGameDetails { get; set; }
+
+    }
+}
