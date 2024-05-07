e1b7574fb53ff1ce453e01321a3e4a2635bdef9b
nil1lik <birliknil@gmail.com>
2024-05-07 19:02:18 +0300

    added service registration

---
+++ DataAccess/DataAccessServiceRegistration.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Concrete.EfCore;
+using DataAccess.Concretes.Context;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public static class DataAccessServiceRegistration
+    {
+        public static IServiceCollection AddDataAccessServices(this IServiceCollection services)
+        {
+            services.AddDbContext<GameStreamDbContext>();
+            services.AddScoped<IGameDal, EfGameDal>();
+            services.AddScoped<IGameDetailDal, EfGameDetailDal>();
+            services.AddScoped<IGameImageDal, EfGameImageDal>();
+            services.AddScoped<IStreamerDal, EfStreamerDal>();
+            services.AddScoped<IStreamingDal, EfStreamingDal>();
+            services.AddScoped<IUserDal, EfUserDal>();
+            services.AddScoped<IUserGameDal, EfUserGameDal>();
+
+            return services;
+        }
+    }
+}
